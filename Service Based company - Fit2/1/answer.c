d617dd8678d6eb422865297babb9a99bd30fa497
sdivakarrajesh <30803637+sdivakarrajesh@users.noreply.github.com>
2018-09-18 16:52:09 +0530

    fixed typo

--- Service Based company - Fit2/1/answer.c
+++ Service Based company - Fit2/1/answer.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 void cellCompete(int arr[],int n){
-for(int days=0;days<n;days++){
-int temp=0;
-for(int index=0;index<7;index++){
+  for(int days=0;days<n;days++){
+  int temp=0;
+  for(int index=0;index<7;index++){
 
-    if(arr[index+1]==temp)
-    {
-    temp = arr[index];
-    arr[index] = 0;
-    }
-    else {
-        temp = arr[index];
-        arr[index] = 1;
-    }
+      if(arr[index+1]==temp)
+      {
+      temp = arr[index];
+      arr[index] = 0;
+      }
+      else {
+          temp = arr[index];
+          arr[index] = 1;
+      }
 
-}
-arr[7] = (temp==0)?0:1;
-}
-for(int i=0;i<8;i++)printf("%d ",arr[i]);
+  }
+  arr[7] = (temp==0)?0:1;
+  }
+  for(int i=0;i<8;i++)printf("%d ",arr[i]);
 
 }
 int main() {
--- Service Based company - Fit2/2/answer-c++.cpp
+++ Service Based company - Fit2/2/answer-c++.cpp
@@ -5,14 +5,18 @@ int main()
 {
     stack<char> s;
     char expression[1000];
-    int length,flag = 1,temp;
+    int length,flag = 1,temp,count=0;
     gets(expression);
+
     for(length=0;expression[length]!='\0';length++);
+
     for(int i=0;i<length;i++){
         if(expression[i]=='{' || expression[i]=='[' || expression[i]=='('){
             s.push(expression[i]);
+            count++;
            }
-        else{
+        else if (expression[i]=='}' || expression[i]==']' || expression[i]==')')
+        {
             if(s.empty()){
                 flag = 0;
                 break;
@@ -23,23 +27,23 @@ int main()
                     temp = s.top();
                     s.pop();
                     if(temp=='['||temp=='('){flag=0;break;}
-                    break;
+
                 case ']':
                     temp = s.top();
                     s.pop();
                     if(temp=='{'||temp=='('){flag=0;break;}
-                    break;
+
                 case ')':
                     temp = s.top();
                     s.pop();
                     if(temp=='{'||temp=='['){flag=0;break;}
-                    break;
+
                 }
             }
         }
     }
     if(!s.empty())flag=0;
-    if(flag==1)printf("Balanced");
+    if(flag==1)printf("Balanced %d",count);
     else printf("Not Balanced");
 
     return 0;
